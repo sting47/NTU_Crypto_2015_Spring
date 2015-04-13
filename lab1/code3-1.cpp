f223ae23101aad8eb1418db849dcdc182d732e35
sting47 <sting47@gmail.com>
2015-04-13 14:36:31 +0800

    finish lab1 problem 1 and 2. 3 half, 4 TODO

---
+++ lab1/code3-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ofstream output;
+	output.open("input3-1.txt");
+	int n = 50000; // generate 50000 numbers as input
+	output << n << endl;
+	int maximum = pow( 2, 30 );
+	for (int i = 0; i < n; i++)
+	{
+		int x = rand() % (maximum+1);
+		output << x << " ";
+	}
+	output << endl;
+	return 0;
+}
